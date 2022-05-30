5e38511d718c6c886324d8ecb462455d3c9f8308
Legeartis <support@legeartis.io>
2022-05-30 13:58:33 +0700

    v2.0.1

--- src/exceptions/USException.php
+++ src/exceptions/USException.php
@@ -8,6 +8,10 @@ class USException extends Exception
 {
     public function __construct(array $error)
     {
-        parent::__construct($error['message'], $error['code']);
+        if (array_key_exists('ErrorResponse', $error)) {
+            parent::__construct($error['ErrorResponse']['message'], $error['ErrorResponse']['code']);
+        } else {
+            parent::__construct($error['message'], $error['code']);
+        }
     }
 }
